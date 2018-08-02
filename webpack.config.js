624d12b98a632b00a59bcd124496232f45879241
Eder Marques <eder.marques@citi.org.br>
2018-08-01 21:22:00 -0300

    Boilerplate webpack + babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,26 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './ex/main.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      }
+    ]
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
